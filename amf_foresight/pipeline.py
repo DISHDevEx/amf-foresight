dbfd541975887a5429062d19d443580ace02a74c
siddhanthsatish <siddhanthsat@umass.edu>
2023-07-12 23:25:14 +0000

    fix: Orchestrator updated

--- amf_foresight/pipeline.py
+++ amf_foresight/pipeline.py
@@ -2,20 +2,21 @@ from load_data import AMFDataProcessor
 from feature_engineering import FeatureEngineer
 import argparse
 
-def run_pipeline(args):
-    processor = AMFDataProcessor()
-    print("Processing Data...")
-    data = processor.get_data(args.directory, args.metric)
-    print("...Completed Processing Data")
-    print(" ")
-    print('Feature Engineering...')
-    feature_engineer = FeatureEngineer()
-    data = feature_engineer.value_modifier(data, args.type)
-    print('...Completed Feature Engineering')
-    print("Summary of Requested data:")
-    print(data.describe())
-    print("First few entries of requested data:")
-    print(data.head())
+class Orchestrator:
+    def run_pipeline(self, args):
+        processor = AMFDataProcessor()
+        print("Processing Data...")
+        data = processor.get_data(args.directory, args.metric)
+        print("...Completed Processing Data")
+        print(" ")
+        print('Feature Engineering...')
+        feature_engineer = FeatureEngineer()
+        data = feature_engineer.value_modifier(data, args.type)
+        print('...Completed Feature Engineering')
+        print("Summary of Requested data:")
+        print(data.describe())
+        print("First few entries of requested data:")
+        print(data.head())
     
     
 if __name__ == "__main__":    
@@ -25,7 +26,9 @@ if __name__ == "__main__":
     parser.add_argument("--pod", type=str, required=False, help="Pod name to filter on.")
     parser.add_argument("--type", type=str, required=False, help="Type of Feature Engineering")
     args = parser.parse_args()
-    run_pipeline(args)
+    
+    orchestra = Orchestrator()
+    orchestra.run_pipeline(args)
     
     
     
