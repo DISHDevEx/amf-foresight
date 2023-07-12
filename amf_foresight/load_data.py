b98c35298bfc956308ab54236ee53c853e838565
siddhanthsatish <siddhanthsat@umass.edu>
2023-07-12 19:04:51 +0000

    feat: added argparse functionality

--- amf_foresight/load_data.py
+++ amf_foresight/load_data.py
@@ -11,22 +11,29 @@ import os
 import json
 import boto3
 import itertools
+import pyspark
+import logging
+import argparse
+
 
 class AMFDataProcessor:
-    
+
     def get_data(self, directory, metric=None, pod=None):
+        """
+        This function takes in the directory of JSON files and returns a combined pandas dataframes
+        :param directory: Path to JSON files
+        """
         spark_dataframes = self.get_dataframes(directory)
         spark_dataframe = self.transform_dataframe(spark_dataframes, metric, pod)
         pandas_dataframe = self.get_values(spark_dataframe)
         return pandas_dataframe
-    
+
     def get_dataframes(self, directory):
         """
         This function extracts spark dataframes from a given directory of JSON files
         :param directory: Path to JSON files
         """
         flag = False
-        print(os.listdir(directory))
         for i, filename in enumerate(os.listdir(directory)):
             if os.path.isfile(os.path.join(directory, filename)) and not flag:
                 appended_df = self.get_amf_data(os.path.join(directory, filename))
@@ -36,7 +43,7 @@ class AMFDataProcessor:
                     new_df = self.get_amf_data(os.path.join(directory, filename))
                     appended_df = appended_df.union(new_df)
         return appended_df
-    
+
     def transform_dataframe(self, amf_data, metric_name= None, pod_name=None):
         """
         This function tranforms the timestamps and filters based on metric_name and pod_name
@@ -50,13 +57,12 @@ class AMFDataProcessor:
         if pod_name:
             amf_data = amf_data.filter(F.col("metric_pod").startswith(pod_name))
         return amf_data
-    
+
     def get_amf_data(self, json_object_path):
         """
         This function extracts the dataframe from JSON and filters out AMF data
         :param json_object_path: Path to JSON
         """
-        # print("file path", json_object_path)
         obj = Nested_Json_Connector(json_object_path)
         err, data = obj.read_nested_json()
         data = data.select('timestamps', 'metric___name__', 'values', 'metric_pod', 'metric_container', 'metric_name', 'metric_namespace')
@@ -86,7 +92,6 @@ class AMFDataProcessor:
 
         x_values = data.select("date_col").rdd.flatMap(lambda x: x).collect()
         y_values = data.select("values").rdd.flatMap(lambda x: x).collect()
-
         x_flat = [item for sublist in x_values for item in sublist]
         y_flat = [item for sublist in y_values for item in sublist]
 
@@ -102,9 +107,31 @@ class AMFDataProcessor:
 
         df = df.groupby('date_col').agg({'values': 'sum'}).reset_index()
         return df
-  
+
 
     def download_chunks(self, local_path):
+        """
+        This function takes in the path to save the chunks and saves the raw data in the given path
+        :param
+        """
         aws_command = f"aws s3 cp s3://open5gs-respons-logs/prometheus-metrics/respons-amf-forecaster/ {local_path} --recursive"
         result = subprocess.run(aws_command, shell=True, check=True)
            
+            
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(description="Process some AMF data.")
+    parser.add_argument("--directory", type=str, required=True, help="Path to JSON files directory.")
+    parser.add_argument("--metric", type=str, required=False, help="Metric name to filter on.")
+    parser.add_argument("--pod", type=str, required=False, help="Pod name to filter on.")
+    
+    args = parser.parse_args()
+    
+    processor = AMFDataProcessor()
+    data = processor.get_data(args.directory, args.metric)
+    print("Summary of Requested data:")
+    print(data.describe())
+    print("First few entries of requested data:")
+    print(data.head())
+    
+
+    
