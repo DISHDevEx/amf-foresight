b835db888b85fe9062eef2de13dd64c36a5116f3
siddhanthsatish <siddhanthsat@umass.edu>
2023-07-21 20:14:51 +0000

    arima class, model selection, model upload, filename updates, training orch, pipeline orch, argparse

---
+++ amf_foresight/arima.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import numpy as np
+import argparse
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+class ARIMAModel:
+    def __init__(self, data):
+        self.dataframe = data
+        self.values = self.dataframe['values'].values
+        self.test_size = 0.2
+        self.forecast_eval_size = 0.1
+        self.model = None
+        self.train, self.test, self.forecast_eval = self.train_test_split()
+    
+    def train_test_split(self):
+        train_size = int(len(self.values) * (1 - self.test_size - self.forecast_eval_size))
+        test_size = int(len(self.values) * (1 - self.forecast_eval_size))
+        train, test, forecast_eval = self.values[0:train_size], self.values[train_size:test_size], self.values[test_size:]
+        return train, test, forecast_eval
+    
+    def fit(self, order):
+        self.model = ARIMA(self.train, order=order)
+        self.model_fit = self.model.fit()
+        return self.model_fit
+
+    def predict(self):
+        self.predictions = self.model_fit.predict(start=len(self.train), end=len(self.train)+len(self.test)-1)
+        return self.predictions
+
+    def evaluate(self):
+        mse = mean_squared_error(self.test, self.predictions)
+        return mse
+
+    def tune_hyperparameters(self):
+        p_values = [1, 2]
+        d_values = [0, 1]
+        q_values = [0, 1, 2]
+        best_mse, best_order = float("inf"), None
+        for p in p_values:
+            for d in d_values:
+                for q in q_values:
+                    order = (p, d, q)
+                    try:
+                        self.fit(order)
+                        predictions = self.predict()
+                        mse = mean_squared_error(self.test, predictions)
+                        if mse < best_mse:
+                            best_mse, best_order = mse, order
+                    except:
+                        continue
+        return best_order
+
+    def run(self):
+        best_order = self.tune_hyperparameters()
+        self.fit(best_order)
+        self.predict()
+        mse = self.evaluate()
+        return best_order, mse
+
+    def forecast(self, steps):
+        if self.model_fit is None:
+            raise Exception('The model must be fit before forecasting.')
+        forecasted_values = self.model_fit.forecast(steps=steps)
+        return forecasted_values
+
+    def evaluate_forecast(self, steps):
+        forecasted_values = self.forecast(steps)
+        mse = mean_squared_error(self.forecast_eval[:steps], forecasted_values)
+        return mse
+
+    def plot(self, forecast_steps):
+        # Extract date values
+        date_values = pd.to_datetime(self.dataframe['date_col'])
+
+        # Create separate arrays for each segment of data (train, test, forecast_eval)
+        train_dates = date_values[:len(self.train)]
+        test_dates = date_values[len(self.train):len(self.train) + len(self.test)]
+        forecast_eval_dates = date_values[len(self.train) + len(self.test):][:forecast_steps]
+
+        plt.figure(figsize=(15, 7))
+
+        # Plot the original data
+        plt.plot(train_dates, self.train, label='Train')
+        plt.plot(test_dates, self.test, label='Test')
+        plt.plot(forecast_eval_dates, self.forecast_eval[:forecast_steps], label='Future true')
+
+        # Forecast and plot the forecast
+        forecasted_values = self.forecast(forecast_steps)
+
+        plt.plot(forecast_eval_dates[:forecast_steps], forecasted_values[:forecast_steps], label='Forecast')
+
+        plt.legend()
+        plt.savefig('arima2.png')
+        plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Feature Engineer AMF data.")
+    parser.add_argument("--data", type=str, required=False, help="Path to filtered AMF data")
+    args = parser.parse_args()
+    dataframe = pd.read_parquet(args.data)
+    
+    arima_model = ARIMAModel(dataframe)
+    
+    best_order, mse = arima_model.run()
+
+    print(f"Best order: {best_order}")
+    print(f"Test MSE: {mse}")
+
+    # Forecast next 5 steps
+    forecasted_values = arima_model.forecast(22)
+    print(f"Forecasted values: {forecasted_values}")
+
+    # Evaluate the forecast
+    forecast_mse = arima_model.evaluate_forecast(22)
+    print(f"Forecast MSE: {forecast_mse}")
+
+    # Plot the data
+    arima_model.plot(22)
+    
--- amf_foresight/pipeline.py
+++ amf_foresight/pipeline.py
@@ -1,12 +1,17 @@
+import pandas as pd
 from load_data import AMFDataProcessor
 from datetime import datetime
 from setup_logger import setup_logger
 from feature_engineering import FeatureEngineer
+from arima import ARIMAModel
+from autoregression import AutoRegressionModel
+from utils import Utils
 import argparse
 import logging
 import time 
 import os
 import sys
+import pickle
 setup_logger()
 
 class Orchestrator:
@@ -15,15 +20,78 @@ class Orchestrator:
     A class that handles the orchestration of preprocessing, training and evaluating AMF data.
     """
     def __init__(self):
+        self.args = args
         self.processor = AMFDataProcessor()
         self.feature_engineer = FeatureEngineer()
-
+        self.utils = Utils()
+        self.data = None
+        self.selected_model = None
+        self.model = None
+    
     def preprocessing(self, args):
         logging.info("Preprocessing Data..")
         raw = self.processor.run(args)
         processed = self.feature_engineer.value_modifier(raw, args.type)
         logging.info("Preprocessed Data.")
         return processed
+    
+    def train(self, args):
+        self.selected_model = args.model
+        logging.info(f"Selected model is {self.selected_model}")
+        if self.selected_model == 'ARIMA':
+            self.model = ARIMAModel(self.data)    
+        if self.selected_model != "PROPHET":
+            logging.info(f"Tuning hyperparamers..")
+            hyper, mse = self.model.run()
+            logging.info(f"Best hyperparameters for this model: {hyper} Test MSE: {mse}")
+        if not os.path.exists("models"):
+            logging.info(f"Creating folder: {folder}")
+            os.makedirs(folder)
+        model_file_path = "models/" + self.selected_model.lower() + ";params:" + str(hyper) + ";time:" + datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        with open(model_file_path, 'wb') as model_file:
+            pickle.dump(self.model.model_fit, model_file)
+        self.utils.upload_file(model_file_path, model_file_path)
+    
+    def save(self, processed):
+        summary_str = processed.describe().to_string().replace('\n', ' | ')
+        head_str = processed.head().to_string().replace('\n', ' | ')
+        logging.info("Summary of Requested data:")
+        logging.info(summary_str)
+        logging.info("First few entries of requested data:")
+        logging.info(head_str)
+        filename = "sample::" + os.path.basename(__file__) + "::metric:" + str(args.metric) + ";pod:" + str(args.pod) + ";level:" + str(args.level) + ";start:" + args.start + ";end:" + args.end
+        path = "parquet/" + filename                                                                                 
+        processed.to_parquet(path, index=False)
+        logging.info(f"(Locally) Data Saved to: {path}")
+        self.utils.upload_file(path, path)
+        return path
+        
+     
+    def run(self, args):
+        """
+        This function runs the entire pipeline 
+        :param
+        """
+        time1 = time.time()
+        processed = None
+        if any([args.download, args.process, args.generate]):
+            processed = self.preprocessing(args)
+        if isinstance(processed, pd.DataFrame):
+            self.data = processed
+            path = self.save(self.data)
+        if args.train:
+            if isinstance(processed, pd.DataFrame):
+                self.data = pd.read_parquet(path)
+            else:
+                self.data = pd.read_parquet(args.data)
+            self.train(args)
+        if args.forecast:
+            mse = self.model.evaluate_forecast(10)
+            self.model.plot(10)
+            
+        
+        
+        
         
         
     
@@ -35,33 +103,43 @@ if __name__ == "__main__":
     parser.add_argument("--download", action='store_true', help="Include this flag to download chunks. --download requires --chunks.")
     parser.add_argument("--process", action='store_true', help="Include this flag to process chunks into JSON format. --process requires --chunks, --jsons, --start, and --end.")
     parser.add_argument("--generate", action='store_true', help="Include this flag to generate the data frame and save the data as a paraquet file. --generate requires --jsons and --level.")
-    parser.add_argument("--chunks", required=True, type=str, help="Path where the chunks are/should be downloaded. The chunks contain the raw data from the AMF.")
-    parser.add_argument("--jsons", required=True, type=str, help="Path where the processed JSONs are/should be stored. These JSONs are generated from the chunks.")
-    parser.add_argument("--parquet", required=True, type=str, help="Path where the processed CSV are/should be stored as a paraquet file. These files are generated from the JSONs.")
-    parser.add_argument("--start", required=True, type=str, help="Start time of the data extraction in the format %%Y-%%m-%%d %%H:%%M:%%S.")
-    parser.add_argument("--end", required=True, type=str, help="End time of the data extraction in the format %%Y-%%m-%%d %%H:%%M:%%S.")
-    parser.add_argument("--level", required=True, type=str, help="Container level to filter on. Could be 'amf', 'support', 'upperlimit', 'amf+support' ")
-    parser.add_argument("--type", type=str, required=False, help="Type of feature you want.")
-    parser.add_argument("--metric", required=False, type=str, help="Metric name to filter on. Leave empty for all metrics.")
-    parser.add_argument("--pod", required=False, type=str, help="Pod name to filter on. Leave empty for all pods.")    
+    parser.add_argument("--train", action='store_true', help="Include this flag to train your data.")
+    parser.add_argument("--forecast", action='store_true', help="Include plot your original data and forecasted data")
+    
+    
+    parser.add_argument("--chunks", type=str, help="Path where the chunks are/should be downloaded. The chunks contain the raw data from the AMF.")
+    parser.add_argument("--jsons", type=str, help="Path where the processed JSONs are/should be stored. These JSONs are generated from the chunks.")
+    parser.add_argument("--parquet", type=str, help="Path where the processed CSV are/should be stored as a paraquet file. These files are generated from the JSONs.")
+    parser.add_argument("--data", type=str, help="Path where the processed CSV are/should be stored as a paraquet file. These files are generated from the JSONs.")
+    parser.add_argument("--start", type=str, help="Start time of the data extraction in the format %%Y-%%m-%%d %%H:%%M:%%S.")
+    parser.add_argument("--end", type=str, help="End time of the data extraction in the format %%Y-%%m-%%d %%H:%%M:%%S.")
+    parser.add_argument("--level", type=str, help="Container level to filter on. Could be 'amf', 'support', 'upperlimit', 'amf+support' ")
+    parser.add_argument("--type", type=str, help="Type of feature you want.")
+    parser.add_argument("--metric", type=str, help="Metric name to filter on. Leave empty for all metrics.")
+    parser.add_argument("--pod", type=str, help="Pod name to filter on. Leave empty for all pods.")    
+    parser.add_argument("--model", type=str, help="Model you would like to use.")
+    
     
     args = parser.parse_args()
     
-    sys.stdout = open('logs/console_output.log', 'w')
-    orchestra = Orchestrator()
-    processed = orchestra.preprocessing(args)
     
-    summary_str = processed.describe().to_string().replace('\n', ' | ')
-    head_str = processed.head().to_string().replace('\n', ' | ')
-
-    logging.info("Summary of Requested data:")
-    logging.info(summary_str)
-    logging.info("First few entries of requested data:")
-    logging.info(head_str)
-
-    filename = "sample::" + os.path.basename(__file__) + "::metric:" + str(args.metric) + ";pod:" + str(args.pod) + ";level:" + str(args.level) + ";time:" + datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    path = "parquet/" + filename                                                                                 
-    processed.to_parquet(path, index=False)
-    logging.info(f"Data Saved to: {path}")
+    if not any([args.download, args.process, args.generate, args.train, args.forecast]):
+        parser.error("One of --download, --process, --generate, --train must be provided.")
+    if args.download and not args.chunks:
+        parser.error("--download requires --chunks.")
+    if args.process and not all([args.chunks, args.jsons, args.start, args.end]):
+        parser.error("--process requires --chunks, --jsons, --start, and --end.")
+    if args.generate and not all([args.jsons, args.parquet, args.level]):
+        parser.error("--generate requires --jsons, --parquet, and --level.")
+    if args.train and not ((all([args.generate, args.model])) or (all([args.data, args.model]))):
+        parser.error("--train requires --data and --model or --generate and --model")
+    if args.forecast and not args.train:
+        parser.error("--forecast requires --train")
+       
+    orchestra = Orchestrator()
+    orchestra.run(args)
+    
+    
+   
     
     
