916c069003bc26d40d287e99540fbb7ba8617197
siddhanthsatish <siddhanthsat@umass.edu>
2023-07-26 16:24:49 +0000

    fix: adding file names, class names and function names to repo

--- amf_foresight/utils.py
+++ amf_foresight/utils.py
@@ -4,7 +4,8 @@ from io import BytesIO
 import logging
 from setup_logger import setup_logger
 from botocore.exceptions import ClientError
-
+import os
+import inspect
 
 setup_logger()
 
@@ -27,7 +28,7 @@ class Utils:
         except ClientError as error:
             logging.error(error)
             return False
-        logging.info(f'(S3) Uploaded File to: s3://{self.bucket_name}/{key}')
+        logging.info(f"{os.path.basename(__file__)}::{self.__class__.__name__}::{inspect.currentframe().f_code.co_name}::(S3) Uploaded File to: s3://{self.bucket_name}/{key}")
         return True
 
 
@@ -37,7 +38,7 @@ class Utils:
         """
         with open(local_path, 'wb') as file:
             self.client.download_fileobj(self.bucket_name, key, file)
-        logging.info(f"Downloaded file to: {local_path}")
+        logging.info(f"{os.path.basename(__file__)}::{self.__class__.__name__}::{inspect.currentframe().f_code.co_name}::Downloaded file to: {local_path}")
         return True
     
     def read_parquet_to_pandas_df(self, key):
@@ -61,6 +62,6 @@ class Utils:
         except ClientError as error:
             logging.error(error)
             return False
-        logging.info(f'(S3) Uploaded Dataframe to: s3://{self.bucket_name}/{key}')
+        logging.info(f"{os.path.basename(__file__)}::{self.__class__.__name__}::{inspect.currentframe().f_code.co_name}::(S3) Uploaded Dataframe to: s3://{self.bucket_name}/{key}")
         return True
     
